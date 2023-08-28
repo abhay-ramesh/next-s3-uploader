35c5ad70434c93629a8f99872252f5473f2a3260
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-08-29 00:11:01 +0530

    second fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["eslint-config-custom"],
+  extends: [],
   settings: {
     next: {
       rootDir: ["apps/*/"],
---
+++ packages/next-s3-uploader/CHANGELOG.md
@@ -0,0 +1,13 @@
+# next-s3-uploader
+
+## 0.1.0
+
+### Minor Changes
+
+- # First Release
+
+  This is the first release of the next-s3-uploader package.
+
+  ## Introduction
+
+  Upload files to S3 from Next.js App Directory.
