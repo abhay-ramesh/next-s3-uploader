6924be92c0a770849c8b709ca7347d7e30e08839
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-10-08 01:07:40 +0530

    Docs API

--- apps/example/next.config.js
+++ apps/example/next.config.js
@@ -3,15 +3,15 @@ const nextConfig = {
     images: {
         domains: ['http://localhost:9000'],
     },
-    webpack: (config, { webpack, isServer, nextRuntime }) => {
-        // Avoid AWS SDK Node.js require issue 
-        // Temporary fix 
-        if (isServer && nextRuntime === "nodejs")
-            config.plugins.push(
-                new webpack.IgnorePlugin({ resourceRegExp: /^(aws-crt|@aws-sdk\/signature-v4-crt)$/ })
-            );
-        return config;
-    },
+    // webpack: (config, { webpack, isServer, nextRuntime }) => {
+    //     // Avoid AWS SDK Node.js require issue 
+    //     // Temporary fix 
+    //     if (isServer && nextRuntime === "nodejs")
+    //         config.plugins.push(
+    //             new webpack.IgnorePlugin({ resourceRegExp: /^(aws-crt|@aws-sdk\/signature-v4-crt)$/ })
+    //         );
+    //     return config;
+    // },
 }
 
 module.exports = nextConfig
